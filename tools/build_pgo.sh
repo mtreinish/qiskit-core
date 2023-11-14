b8a46497e53f1784e88c79a63bcd0e5a8dd141f6
Matthew Treinish <mtreinish@kortar.org>
2023-11-14 17:12:53 -0500

    Fixes from testing

--- tools/build_pgo.sh
+++ tools/build_pgo.sh
@@ -3,8 +3,8 @@
 # Create venv for instrumented build and test
 python -m venv build_pgo
 # Build with instrumentation
-RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data -Ctarget-cpu=x86-64-v3" build_pgo/bin/pip install -c constraints.txt .
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" build_pgo/bin/pip install -c constraints.txt .
 build_pgo/bin/pip install -c constraints.txt -r requirements-dev.txt
 # Run profile data generation
 
-stestr run
+build_pgo/bin/stestr run
