dfc3031910b5a38dfc421efd159b8fd74604cdf0
Matthew Treinish <mtreinish@kortar.org>
2023-11-14 17:20:32 -0500

    More fixes

--- tools/build_pgo.sh
+++ tools/build_pgo.sh
@@ -2,9 +2,21 @@
 
 # Create venv for instrumented build and test
 python -m venv build_pgo
+
+python -c import sys;assert sys.platform == "win32"
+win32=$?
+if win32 == 0; then
+    source build_pgo/Scripts/activate
+else
+    source build_pgo/bin/activate
+fi
+
 # Build with instrumentation
-RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" build_pgo/bin/pip install -c constraints.txt .
-build_pgo/bin/pip install -c constraints.txt -r requirements-dev.txt
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" pip install -c constraints.txt -e .
+RUSTFLAGS="-Cprofile-generate=/tmp/pgo-data" python setup.py build_rust --release --inplace
+pip install -c constraints.txt -r requirements-dev.txt
 # Run profile data generation
 
-build_pgo/bin/stestr run
+stestr run
+
+deactivate
