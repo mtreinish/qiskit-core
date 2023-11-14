972e4589e98b4bde150d0b409a07d9aa8002ea2a
Matthew Treinish <mtreinish@kortar.org>
2023-11-14 17:26:44 -0500

    Fixes again

--- tools/build_pgo.sh
+++ tools/build_pgo.sh
@@ -5,9 +5,9 @@ set -xe
 # Create venv for instrumented build and test
 python -m venv build_pgo
 
-python -c 'import sys;assert sys.platform == "win32"'
-win32=$?
-if win32 == 0; then
+python -c 'import sys;assert sys.platform == "win32"' || true
+is_win=$?
+if [[ $is_win -eq 0 ]]; then
     source build_pgo/Scripts/activate
 else
     source build_pgo/bin/activate
